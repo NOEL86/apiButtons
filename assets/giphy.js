ba357b244c1ead2f8e546363ce16db66008b0ec7
Ashley <anoelj86@yahoo.com>
2018-05-23 21:37:29 -0600

    array additions working

--- assets/giphy.js
+++ assets/giphy.js
@@ -1,49 +1,84 @@
-var shows = ['Game of Thrones', 'Outlander', 'Gilmore Girls', 'The Big Bang Theory', 'Friends', 'Buffy the Vampire Slayer', 'Smallville'];
-var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=DPVJwGqSewzajKIgDz9FLExLVWSPBrtL&q=&limit=25&offset=0&rating=G&lang=en?=";
+var showsArray = ['Game of Thrones', 'Outlander', 'Gilmore Girls', 'The Big Bang Theory', 'Friends', 'Smallville'];
 var API = "DPVJwGqSewzajKIgDz9FLExLVWSPBrtL";
-var rating = "g";
+var rating = "G";
 var numberRequest = 10;
-var q = "q";
+var enteredSearch = "";
+var arrayItem = [];
+
+
 
 $(document).ready(function () {
 
-    var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=DPVJwGqSewzajKIgDz9FLExLVWSPBrtL&q=&limit=25&offset=0&rating=G&lang=en?=";
+    for (var i = 0; i < showsArray.length; i++) {
+        var search = $("<button>" + showsArray[i] + "</button>");
+        search.appendTo("#buttons");
 
-    queryUrl += '?' + $.param({
-        'api-key': "DPVJwGqSewzajKIgDz9FLExLVWSPBrtL",
-        'q': q,
-    });
+    }
+
+    // var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=DPVJwGqSewzajKIgDz9FLExLVWSPBrtL&q=" + enteredSearch + "& limit=25 & offset=0 & rating=G & lang=en";
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=DPVJwGqSewzajKIgDz9FLExLVWSPBrtL&q=Game+Of+Thrones& limit=25 & offset=0 & rating=G & lang=en";
 
     $.ajax({
-        URL: queryUrl,
-        Method: "GET",
+        url: queryUrl,
+        method: "GET",
     }).then(function (response) {
         console.log(response);
-    })
-})
+
+        $(".btn").on("click", function () {
+            event.preventDefault();
+
+            var enteredSearch = $("#enteredSearch").val().trim();
+            $("enteredSearch").val(" ");
+            console.log(enteredSearch);
+
+            showsArray.push(enteredSearch);
+            console.log(showsArray);
 
 
-// for (var i = 0; i < shows.length; i++) {
-//     var shows = $("<button>");
-//     $("#buttons").append(shows);
+            // showsArray.splice(index, 1);
 
-//     $(".btn").on(click, function () {
-//         var newButton = $("enteredSearch")
-//         newButton = $("<button>");
-//         $("gifs-appear-here").append(newButton);
-//         console.log(enteredSearch);
-//     })
+            // var responseGif = response.data.images;
+            // var showImage = $("<img>");
+            // showImage.attr("src", responseGif);
 
-
-
-// };
+            // $("#gifs-appear-here").append(showImage);
 
 
 
 
 
 
-// })
+
+        });
+
+
+
+
+
+    });
+
+
+
+
+});
+
+
+
+
+
+
+
+
+// if (state === "still") {
+//     $(this).attr("src", $(this).attr("data-animate"));
+//     $(this).attr("data-state", "animate");
+//   } else {
+//     $(this).attr("src", $(this).attr("data-still"));
+//     $(this).attr("data-state", "still");
+//   }
+// });
+
+
 
 // $(".gif").on("click", function () {
 
