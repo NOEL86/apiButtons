f802a0535c979a30bd51f5b284913d8f7d2ad8c2
Ashley <anoelj86@yahoo.com>
2018-05-30 21:12:36 -0600

    finished

--- assets/giphy.js
+++ assets/giphy.js
@@ -1,4 +1,5 @@
 
+
 $(document).ready(function () {
     var showsArray = ['Game of Thrones', 'Outlander', 'Gilmore Girls', 'The Big Bang Theory', 'Friends', 'Smallville'];
     var api = "api_key=rK3wU1cHmuBEy5fHHQ9zvm9qbyfNd45i&q=";
@@ -7,14 +8,6 @@ $(document).ready(function () {
     var q = "";
     var queryUrl = "https://api.giphy.com/v1/gifs/search?&" + api + q + "&limit=12&" + offset + rating + "&lang=en";
     var offset = Math.floor((Math.random() * 10) * 10);
-
-    $.ajax({
-        url: queryUrl,
-        method: "GET",
-    }).then(function (response) {
-        console.log(response);
-    });
-
 
     //create new buttons for shows to search//
     function renderButtons() {
@@ -35,7 +28,7 @@ $(document).ready(function () {
     renderButtons();
 
     //search for the entered button gifs and have the gifs appear//
-    $(".gifSelected").on("click", function () {
+    $("#buttons").on("click", ".gifSelected", function () {
         event.preventDefault();
         q = $(this).attr("data-name");
         console.log(q);
@@ -51,36 +44,21 @@ $(document).ready(function () {
             }).then(function (response) {
                 console.log(response);
 
-
                 for (var i = 0; i < response.data.length; i++) {
 
                     var showImage = $("<img>");
                     showImage.addClass("gif");
                     showImage.attr("src", response.data[i].images.fixed_height.url, response.data[i].images.fixed_height_still.url);
 
+                    showImage.attr("data-animate", response.data[i].images.fixed_height.url)
+                    showImage.attr("data-still", response.data[i].images.fixed_height_still.url)
+                    showImage.attr("data-state", "animated")
+
                     //need both things to change image from still to animate
 
                     $("#gifs-appear-here").prepend(showImage);
                     console.log(showImage);
 
-                    $(".gif").on("click", function (event) {
-                        debugger;//shows events triggered to determine selected items
-
-                        var animate = $(".gif").attr("data-animate");
-                        // var still = $(".gif").attr("data-still");
-
-                        var state = $(this).attr("data-state");
-                        console.log(this);
-
-                        if (state === "still") {
-                            $(this).attr("src", $(this).attr("data-animate"));
-                            $(this).attr("data-state", "animate");
-                        } else {
-                            $(this).attr("src", $(this).attr("data-still"));
-                            $(this).attr("data-state", "still");
-                        }
-
-                    });
 
                 }
 
@@ -90,12 +68,28 @@ $(document).ready(function () {
 
     });
 
+    $("#gifs-appear-here").on("click", ".gif", function (event) {
+        debugger;//shows events triggered to determine selected items
+
+        var animate = $(this).attr("data-animate");
+
+        var state = $(this).attr("data-state");
+        console.log(this);
+
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+
+    });
+
     //Add new searches to array//
     $(".btn").on("click", function (event) {
         event.preventDefault();
         var enteredSearch = $("#enteredSearch").val().trim();
-
-
 
         if (enteredSearch === "") {
             return;
